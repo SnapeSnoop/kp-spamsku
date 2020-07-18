617dd54670881c6ba9f0b9930843cd54dc53b361
ffadilaputra <i.fadilaputra@gmail.com>
2020-07-18 12:51:17 +0700

    test env

--- application/models/m_login.php
+++ application/models/m_login.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_login extends CI_Model {
 
+	//test
+
 	function login_admin($username,$password){
 		$query = $this->db->query("SELECT * from tb_admin where username='$username' and password='$password'");
 		return $query;
