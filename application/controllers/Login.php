37caaac320e74ee9c9d01acd86180934ef35b7d9
ffadilaputra <i.fadilaputra@gmail.com>
2020-07-18 12:58:10 +0700

    test lowercase

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,7 +5,7 @@ class Login extends CI_Controller {
 
 	function __construct(){
 		parent::__construct();
-		$this->load->model('M_login','m_login');
+		$this->load->model('Model_login','m_login');
 	}
 
 	function index(){
--- application/models/Model_login.php
+++ application/models/Model_login.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_login extends CI_Model {
+class Model_login extends CI_Model {
 
 	//testss
 
