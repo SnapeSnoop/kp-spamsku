25ff3ce80e0e8432f8a8e2def5d7ec6f1d71fe81
ekamahendrabagaskara@gmail.com <ekamahendrabagaskara@gmail.com>
2020-07-18 14:24:27 +0700

    try

--- application/controllers/Utama.php
+++ application/controllers/Utama.php
@@ -6,7 +6,7 @@ class Utama extends MY_Controller {
 	function index(){
 
 		$this->load->model('Model_pelanggan','m_pelanggan');
-		$this->load->model('Model_tarig','m_tarif');
+		$this->load->model('Model_tarif','m_tarif');
 		$this->load->model('Model_pembayaran','m_pembayaran');
 		$this->load->model('Model_pengaduan','m_pengaduan');
 
